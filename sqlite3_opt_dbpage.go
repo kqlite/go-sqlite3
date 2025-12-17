b4f1e8c258b84511e0955309fda317b9082a47d8
Emil Penchev <emil.penchev@suse.com>
2025-12-17 17:04:54 +0200

    Added sqlite_rsync functions.

--- sqlite3_opt_dbpage.go
+++ sqlite3_opt_dbpage.go
@@ -2,14 +2,71 @@
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
-//go:build sqlite_dbpage
-// +build sqlite_dbpage
+
+//go:build sqlite_dbpage dbpage
+// +build sqlite_dbpage dbpage
 
 package sqlite3
 
 /*
 #cgo CFLAGS: -DSQLITE_ENABLE_DBPAGE_VTAB=1
 #cgo LDFLAGS: -lm
+
+#define SQLITE_RSYNC_NO_MAIN
+#define SQLITE_RSYNC_USE_H
+
+#include "sqlite_rsync.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 */
 import "C"
 
+import (
+    "os"
+    "errors"
+	"unsafe"
+)
+
+func RsyncOrigin(dbPath string, pIn, pOut *os.File) error {
+    cOriginPath := C.CString(dbPath)
+	defer C.free(unsafe.Pointer(cOriginPath))
+
+    ctx := C.SQLiteRsync{
+		zOrigin:    cOriginPath,
+		iProtocol:  C.PROTOCOL_VERSION,
+		isRemote:   1,
+        pIn:        C.fdopen((C.int)(pIn.Fd()), C.CString("rb")),
+        pOut:       C.fdopen((C.int)(pOut.Fd()), C.CString("wb")),
+	}
+
+    C.originSide(&ctx)
+
+    if ctx.nErr > 0 {
+        return errors.New("RSyncOrigin faild")
+    }
+
+    return nil
+}
+
+func RsyncReplica(replicaPath string, pIn, pOut *os.File) error {
+    cReplicaPath := C.CString(replicaPath)
+	defer C.free(unsafe.Pointer(cReplicaPath))
+
+    ctx := C.SQLiteRsync{
+		zReplica:   cReplicaPath,
+		iProtocol:  C.PROTOCOL_VERSION,
+		isRemote:   1,
+        pIn:        C.fdopen((C.int)(pIn.Fd()), C.CString("rb")),
+        pOut:       C.fdopen((C.int)(pOut.Fd()), C.CString("wb")),
+	}
+
+    C.replicaSide(&ctx)
+    
+    if ctx.nErr > 0 {
+        return errors.New("RSyncReplica faild")
+    }
+
+    return nil
+}
